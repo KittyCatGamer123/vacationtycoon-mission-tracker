99cbfc8a39a697afc98e7cebcc03460bf1b90891
Catster <60274720+KittyCatGamer123@users.noreply.github.com>
2022-10-06 18:19:34 +0100

    The Beginning!! (Added files)

---
+++ config.js
@@ -0,0 +1,23 @@
+var POWERS = ['K', 'M', 'B', 'T', 'AA', 'BB', 'CC', 'DD', 'EE', 'FF', 'GG', 'HH', 'II', 'JJ', 'KK', 'LL', 'MM', 'NN', 'OO', 'PP', 'QQ', 'RR', 'SS', 'TT', 'UU', 'VV', 'WW', 'XX', 'YY', 'ZZ', 'AAA', 'BBB', 'CCC', 'DDD', 'EEE', 'FFF', 'GGG', 'HHH', 'III', 'JJJ', 'KFC', 'LLL', 'MMM', 'NNN', 'OOO', 'PPP', 'QQQ', 'RRR', 'SSS', 'TTT', 'UUU', 'VVV', 'WWW', 'XXX', 'YYY', 'ZZZ', 'AAAA', 'BBBB', 'CCCC', 'DDDD', 'EEEE', 'FFFF', 'GGGG', 'HHHH', 'IIII', 'JJJJ', 'KKKK', 'LLLL', 'MMMM', 'NNNN', 'OOOO', 'PPPP', 'QQQQ', 'RRRR', 'SSSS', 'TTTT', 'UUUU', 'VVVV', 'WWWW', 'XXXX', 'YYYY', 'ZZZZ', 'AAAAA', 'BBBBB', 'CCCCC', 'DDDDD', 'EEEEE', 'FFFFF', 'GGGGG', 'HHHHH', 'IIIII', 'JJJJJ', 'KKKKK', 'LLLLL', 'MMMMM', 'NNNNN', 'OOOOO', 'PPPPP', 'QQQQQ', 'RRRRR', 'SSSSS', 'TTTTT', 'UUUUU', 'VVVVV', 'WWWWW', 'XXXXX', 'YYYYY', 'ZZZZZ'];
+
+// Theme ID != Balance ID
+// This is exclusively seen in Ages for now.
+var THEME_ID_OVERRIDES = {};
+
+// Version for last balance update
+// Only includes changes to the actual balance, per the interests of the community.
+var BALANCE_UPDATE_VERSION = 
+{
+  "main": "2.2"
+};
+
+// For game-specific documentation and social channels
+var SOCIAL_HELP_URLS = {
+  "faq": "https://docs.google.com/document/d/1lCa0GNHjXOSpeCgMtqESU7i1UA8LUmObh1s0EfuUPHU/",
+  "discord": "https://discord.gg/vrEUakbBM9",
+  "reddit": "https://www.reddit.com/r/vacationtycoon/"
+}
+
+// If not undefined, will show a datamining warning at the top of the Tracker
+var DATAMINE_WARNING_MIN_RANK = undefined; // For motherland
+var DATAMINE_WARNING_THEME_ID = undefined; // For events
