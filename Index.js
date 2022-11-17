f7d5f625fc41acd95c34e4dbe86533cfe1c74571
Catster <60274720+KittyCatGamer123@users.noreply.github.com>
2022-11-17 11:26:20 +0000

    Number to Time readability

--- Index.js
+++ Index.js
@@ -21,3 +21,22 @@ function NewMenu(Text)
     MenuTitle.innerText = String(Text);
     DocBody.appendChild(MenuTitle);
 }
+
+function TimeToText(Time)
+{
+    let levels = 
+    [
+        [Math.floor(seconds / 31536000), 'y'],
+        [Math.floor((seconds % 31536000) / 86400), 'd'],
+        [Math.floor(((seconds % 31536000) % 86400) / 3600), 'h'],
+        [Math.floor((((seconds % 31536000) % 86400) % 3600) / 60), 'm'],
+        [(((seconds % 31536000) % 86400) % 3600) % 60, 's'],
+    ];
+    let returntext = '';
+    
+    for (let i = 0, max = levels.length; i < max; i++) {
+        if ( levels[i][0] === 0 ) continue;
+        returntext += ' ' + levels[i][0] + ' ' + (levels[i][0] === 1 ? levels[i][1].substr(0, levels[i][1].length-1): levels[i][1]);
+    };
+    return returntext.trim();
+}
